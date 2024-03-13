228fd085e16611e053d28f2368f12b72548230a3
Ahmed Shehab <142121665+AhmedShehab1@users.noreply.github.com>
2024-03-13 23:39:15 +0200

    .

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(Ahmed):
     for i, char in enumerate(Ahmed):
         if ord(char) > 96 and ord(char) < 123:
-            print("{}".format(chr(ord(char) - 32)), end="")
-        else :
-            print("{}".format(char), end="")
+            print("{}{}".format(chr(ord(char) - 32), "\n" if i + 1 == len(Ahmed) else ""), end="")
+        else:
+            print("{}{}".format(char, "\n" if i + 1 == len(Ahmed) else ""), end="")
