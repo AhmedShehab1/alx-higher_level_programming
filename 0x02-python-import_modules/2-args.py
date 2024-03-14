91515cd2cb5462ca2aefcf65ec68a4df9ff91f92
Ahmed Shehab <142121665+AhmedShehab1@users.noreply.github.com>
2024-03-14 23:16:33 +0200

    .

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
     if len(sys.argv) - 1 == 0:
         print("{} arguments.".format(0))
     else:
-        print("{} {}".format(len(sys.argv) - 1, 
-            "argument:" if len(sys.argv) - 1 == 1 else 
+        print("{} {}".format(len(sys.argv) - 1,
+            "argument:" if len(sys.argv) - 1 == 1 else
                 "arguments:"))
         for i, j in enumerate(sys.argv):
             if i == 0:
