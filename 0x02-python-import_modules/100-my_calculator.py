45d904d9ef4fdcff295e9718e2b790e87373ae01
Ahmed Shehab <142121665+AhmedShehab1@users.noreply.github.com>
2024-03-14 23:30:45 +0200

    .

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,17 +8,17 @@ if __name__ == "__main__":
         exit(1)
     if av[2] != '+' and\
         av[2] != '-' and\
-        av[2] != '*' and\
-        av[2] != '/':
+            av[2] != '*' and\
+            av[2] != '/':
         print("Unknown operator. Available operators:\
  +, -, * and /")
         exit(1)
-    print("{} {} {} = {}"\
-        .format(av[1], av[2],\
-            av[3], cc.add(int(av[1]),\
-            int(av[3])) if av[2] == '+'\
-            else cc.sub(int(av[1]), int(av[3]))\
-            if av[2] == '-' else\
-            cc.mul(int(av[1]), int(av[3]))\
-            if av[2] == '*' else\
-            cc.div(int(av[1]), int(av[3]))))
+    print("{} {} {} = {}"
+          .format(av[1], av[2],
+                  av[3], cc.add(int(av[1]),
+                                int(av[3])) if av[2] == '+'
+                  else cc.sub(int(av[1]), int(av[3]))
+                  if av[2] == '-' else
+                  cc.mul(int(av[1]), int(av[3]))
+                  if av[2] == '*' else
+                  cc.div(int(av[1]), int(av[3]))))
