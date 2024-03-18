56e8e204fb1d1a068d105f0c293be36874205f4a
AhmedShehab1 <legendbtw2003@gmail.com>
2024-03-18 22:39:37 +0000

    Edited

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     Len = len(my_list)
     for i in range(Len):
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for List in matrix:
         for int in List:
