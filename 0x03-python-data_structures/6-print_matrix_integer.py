3edd786b5d8d0f1a8eb03c4a341a486e38dcd7b4
Ahmed Shehab <142121665+AhmedShehab1@users.noreply.github.com>
2024-03-19 07:51:58 +0200

    .

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for List in matrix:
         for int in List:
             if len(List) - 1 != List.index(int):
-                print("{}".format(int), end=" ")
+                print("{:d}".format(int), end=" ")
             else:
-                print("{}".format(int), end="")
+                print("{:d}".format(int), end="")
         print("")
