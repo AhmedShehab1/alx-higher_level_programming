42dafd60c170c6cf0c4426751bc5bfe1b6703e1e
Ahmed Shehab <142121665+AhmedShehab1@users.noreply.github.com>
2024-03-19 07:50:12 +0200

    .

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if my_list == []:
+        return my_list
     Len = len(my_list)
     for i in range(Len):
         print("{:d}".format(my_list[Len - (i + 1)]))
