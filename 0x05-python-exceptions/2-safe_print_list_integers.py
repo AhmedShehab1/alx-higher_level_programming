f3ba2f1f83ff13a18761aff7b88919f3feebdc66
AhmedShehab1 <legendbtw2003@gmail.com>
2024-04-02 19:59:50 +0000

    Updated Tasks

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-        Count = 0
-        try:
-                for i in range(x):
-                        if type(my_list[i]) != int:
-                                continue
-                        print("{:d}".format(my_list[i]), end="")
-                        Count += 1
-                print("")
-                return (Count)
-        finally:
-                pass
+    Count = 0
+    try:
+        for i in range(x):
+            if type(my_list[i]) != int:
+                continue
+            print("{:d}".format(my_list[i]), end="")
+            Count += 1
+        print("")
+        return (Count)
+    finally:
+        pass
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-        x = 0
-        new_list = []
-        while (x < list_length + 1):
-                try:
-                        new_list.append(my_list_1[x] / my_list_2[x])
-                except TypeError:
-                        new_list.append(0)
-                        print("wrong type")
-                except ZeroDivisionError:
-                        new_list.append(0)
-                        print("division by zero")
-                except IndexError:
-                        new_list.append(0)
-                        print("out of range")
-                finally:
-                        x += 1
-                        if x == list_length:
-                                return new_list
-                
+    x = 0
+    new_list = []
+    while (x < list_length + 1):
+        try:
+            new_list.append(my_list_1[x] / my_list_2[x])
+        except TypeError:
+            new_list.append(0)
+            print("wrong type")
+        except ZeroDivisionError:
+            new_list.append(0)
+            print("division by zero")
+        except IndexError:
+            new_list.append(0)
+            print("out of range")
+        finally:
+            x += 1
+            if x == list_length:
+                return new_list
