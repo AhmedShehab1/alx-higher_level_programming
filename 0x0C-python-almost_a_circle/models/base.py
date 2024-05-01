6d6f626329a04f8ef6e459529b3293a2de3a6878
Ahmed Shehab <142121665+AhmedShehab1@users.noreply.github.com>
2024-05-02 00:08:36 +0300

    Edited

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -57,3 +57,51 @@ class Base:
                 for obj in list_objs:
                     list_dictionaries.append(obj.to_dictionary())
                 file.write(Base.to_json_string(list_dictionaries))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Method To Convert JSON Representation To
+        Python Data Structure
+        Args:
+            json_string (JSON): JSON Representation
+
+        Returns:
+            Dictionary: Python Data Structure
+        """
+        if json_string is None or not len(json_string) > 0:
+            return list([])
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        Creates a An Object
+        Setting its attribute included in **dictionary
+        Returns:
+            object: Created Object with attributes set
+        """
+        dummy_obj = cls(2, 9)
+        dummy_obj.update(**dictionary)
+        return dummy_obj
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        Loads JSON Object Stored In a File
+        and converts it to Python Equivalent Dictionary
+        That Contains attributes of objects in order
+        to be used to instantiate
+        Returns:
+            list: List With New Objects
+        """
+        list_of_objects = []
+        try:
+            with open(f"{cls.__name__}.json", encoding="utf-8") as file:
+                temp_list = cls.from_json_string(file.read())
+                for dictionary in temp_list:
+                    list_of_objects.append(cls.create(**dictionary))
+        except FileNotFoundError:
+            pass
+        finally:
+            return list_of_objects
