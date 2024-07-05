8b851812ae40f4101e8e044efe69897c336908d4
AhmedShehab1 <legendbtw2003@gmail.com>
2024-07-05 02:57:02 +0000

    .

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Size in Bytes Of the response
 curl -s -o /dev/null -w "%{size_download}\n" $1
-
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Send a delete request to the URL and display the response message
 curl -s -X DELETE $1
-
