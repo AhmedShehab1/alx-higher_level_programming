785935ffdb4bea80bc64ce3a53034307789bcd5d
Ahmed Shehab <142121665+AhmedShehab1@users.noreply.github.com>
2024-08-10 07:18:54 +0300

    .

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
-let y = 1;
-let x;
 exports.addMeMaybe = function (number, theFuntion) {
-  if (y === 1) {
-    x = number;
-    y++;
-  }
-  theFuntion(++x);
+  theFuntion(++number);
 };
