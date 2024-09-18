090065a5bc9ea3f1e8389d1b7c1f69fce2bc5533
AhmedShehab1 <legendbtw2003@gmail.com>
2024-09-18 19:45:04 +0000

    .

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,4 +3,4 @@ USE hbtn_0c_0;
 
 ALTER TABLE first_table COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE first_table MODIFY COLUMN name COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
