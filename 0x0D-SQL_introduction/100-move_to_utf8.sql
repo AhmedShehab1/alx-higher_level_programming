186b5fb2d738e7a23627d7f9d7e0bd36e4e1cc39
AhmedShehab1 <legendbtw2003@gmail.com>
2024-09-18 19:42:43 +0000

    .

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,6 @@
 -- Changing character set and collation.
 USE hbtn_0c_0;
 
-ALTER TABLE first_table  COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE first_table MODIFY name COLLATE utf8mb4_unicode_ci;
