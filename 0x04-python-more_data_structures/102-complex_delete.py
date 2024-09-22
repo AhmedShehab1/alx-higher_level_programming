fbb3ad7c7064a7883c9155da689523b6f2870989
AhmedShehab1 <legendbtw2003@gmail.com>
2024-09-22 16:41:26 +0000

    .

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def complex_delete(a_dictionary, value):
+    if a_dictionary:
+        to_be_deleted = [k for k, v in a_dictionary.items() if v == value]
+        for k in to_be_deleted:
+            del a_dictionary[k]
+        return to_be_deleted
